92d92ca05687abd9634ea630bdcf3e68ae8d039b
Aryaman <aryaman1802@gmail.com>
2025-04-26 03:25:46 -0400

    c++ stl

---
+++ cpp_stl/stl_array.cpp
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>   // this contains all necessary headers
+#include <array>       // the one we need for only arrays
+using namespace std;
+
+/*
+STL = Standard Template Library
+
+- Arrays
+- Vector
+- Deque
+- List
+- Stack
+- Queue
+- Priority Queue
+- Set
+- Map
+- STL algorithms
+*/
+
+int main(){
+    return 0;
+}
