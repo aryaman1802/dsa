78e20e3358fce7e217c6b25c1f3bc21780ab1d16
Aryaman <aryaman1802@gmail.com>
2025-05-03 22:56:45 -0400

    added stack and queue

--- 0_Intro_Problems/cpp_stl/stl_queue.cpp
+++ 0_Intro_Problems/cpp_stl/stl_queue.cpp
@@ -1,8 +1,28 @@
-// #include <bits/stdc++.h>   // this contains all necessary headers
-#include <queue>       // the one we need for only queues
+// #include <bits/stdc++.h>
+#include <queue>
 #include <iostream>
 using namespace std;
 
+// Queue follows FIFO - First In First Out
+// All queue operations happen in O(1)
+
 int main(){
-    return 0;
+    queue<int> q;
+    q.push(1);          // {1}
+    q.push(5);          // {5,1}
+    q.emplace(9);       // {9,5,1}
+    q.emplace(4);       // {4,9,5,1}
+
+    cout << q.back() << " " << q.front() << "\n";
+
+    q.back() += 2;      // {6,9,5,1}
+    q.front() += 3;     // {6,9,5,4}
+
+    cout << q.back() << " " << q.front() << "\n";
+
+    q.pop();            // {6,9,5}
+
+    cout << q.back() << " " << q.front() << "\n";
+
+    // size, swap, and empty - same as Stack
 }
--- 0_Intro_Problems/cpp_stl/stl_stack.cpp
+++ 0_Intro_Problems/cpp_stl/stl_stack.cpp
@@ -1,8 +1,29 @@
-// #include <bits/stdc++.h>   // this contains all necessary headers
-#include <stack>       // the one we need for only stacks
+// #include <bits/stdc++.h>
+#include <stack>
 #include <iostream>
 using namespace std;
 
+// Stack follows LIFO - Last In First Out
+// All stack operations happen in O(1)
+
 int main(){
-    return 0;
+    stack<int> s;
+    s.push(1);          // {1}
+    s.push(2);          // {2,1} 
+    s.push(3);          // {3,2,1}
+    s.push(4);          // {4,3,2,1}
+    s.emplace(5);       // {5,4,3,2,1}
+
+    cout << s.top() << "\n";    // 5
+    // s[i] is invalid
+
+    s.pop();          // {4,3,2,1}
+
+    cout << s.size() << "\n"
+         << s.top() << "\n"
+         << s.empty() << "\n";
+
+    stack<int> s1, s2;
+    s1.swap(s2);
+
 }
