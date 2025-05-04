bf596934efc2ae4b3e3dcf732a2cfe951cdf19df
Aryaman <aryaman1802@gmail.com>
2025-05-03 23:32:33 -0400

    added unordered set

---
+++ 0_Intro_Problems/cpp_stl/stl_unord_set.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // everything is the same as set
+    // only that unordered set stores elements in unordered manner
+
+    unordered_set<int> s;
+}
