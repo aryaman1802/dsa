9bb792ca8cf43cce44190560563a620eb004c9a2
Aryaman <aryaman1802@gmail.com>
2025-05-03 23:05:58 -0400

    added priority queue

--- 0_Intro_Problems/cpp_stl/stl_priority_queue.cpp
+++ 0_Intro_Problems/cpp_stl/stl_priority_queue.cpp
@@ -1,8 +1,30 @@
-// #include <bits/stdc++.h>   // this contains all necessary headers
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+/*
+Push = O(log n)
+Top = O(1)
+Pop = O(log n)
+*/
+
 int main(){
-    return 0;
+    // Maximum Heap
+    priority_queue<int> pq;
+    pq.push(5);         // {5}
+    pq.push(8);         // {8,5}
+    pq.emplace(2);      // {8,5,2}
+    pq.emplace(10);     // {10,8,5,2}
+    cout << pq.top() << "\n";    // 10
+    pq.pop();           // {8,5,2}
+    cout << pq.top() << "\n";   // 8
+
+    // Minimum Heap
+    priority_queue<int, vector<int>, greater<int>> pq2;
+    pq.push(5);         // {5}
+    pq.push(8);         // {5,8}
+    pq.emplace(2);      // {2,5,8}
+    pq.emplace(10);     // {2,5,8,10}
+    cout << pq.top() << "\n";    // 2
+    pq.pop();           // {5,8,10}
+    cout << pq.top() << "\n";   // 5
 }
