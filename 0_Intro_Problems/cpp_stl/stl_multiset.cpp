64fa3d0a4bdf57bb5212fc059be7cb226314f252
Aryaman <aryaman1802@gmail.com>
2025-05-03 23:30:48 -0400

    added multiset

---
+++ 0_Intro_Problems/cpp_stl/stl_multiset.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // everything is the same as set
+    // only that multiset also stores duplicate elements
+
+    multiset<int> ms;
+    ms.insert(1);      // {1}
+    ms.insert(2);      // {1,2}
+    ms.insert(1);      // {1,1,2}
+    ms.insert(3);      // {1,1,2,3}
+    ms.insert(2);      // {1,1,2,2,3}   -- elements are stored in sorted order
+
+    ms.erase(1);     // all 1s will be erased
+
+    ms.erase(ms.find(2));    // only one 2 will be erased
+}
