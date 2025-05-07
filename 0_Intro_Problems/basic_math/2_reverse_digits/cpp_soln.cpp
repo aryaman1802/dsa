ad960d02085640ac5e79dd8685be32df1d3e8f84
Aryaman <aryaman1802@gmail.com>
2025-05-07 01:18:52 -0400

    c++ solution

--- 0_Intro_Problems/basic_math/2_reverse_digits/cpp_soln.cpp
+++ 0_Intro_Problems/basic_math/2_reverse_digits/cpp_soln.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int reverse_num(int n){
-    int r=0;
-    bool sign_flag=false;
-    if(n < 0){
-        sign_flag = true;
+    int r=0, sign=1;
+    if(n<0){
+        sign = -1;
     }
     n = abs(n);
     while(n>0){
+        if((r > (INT16_MAX/10)) || (r < (INT16_MIN/10))){
+            return 0;
+        }
         r = r*10 + n%10;
         n /= 10;
     }
-    if(sign_flag){
-        r *= -1;
-    }
+    r *= sign;
     return r;
 }
 
