75345edc03d9f2106c57fa550da5e659643029df
Aryaman <aryaman1802@gmail.com>
2025-05-07 01:19:02 -0400

    python soln

--- 0_Intro_Problems/basic_math/2_reverse_digits/python_soln.py
+++ 0_Intro_Problems/basic_math/2_reverse_digits/python_soln.py
@@ -1,13 +1,17 @@
 def rev_num(n):
     r = 0
-    sign_flag = True if n<0 else False
+    sign = -1 if n<0 else 1
     n = abs(n)
     while n>0:
         r = r*10 + n%10
         n = n//10
-    if sign_flag:
-        r *= -1
-    return r
+    r *= sign
+    int_max = (2**31) - 1
+    int_min = -(2**31)
+    if (r > int_max) or (r < int_min):
+        return 0
+    else:
+        return r
 
 
 if __name__ == "__main__":
