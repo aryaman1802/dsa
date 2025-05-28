d3e722a8cb82dec1e8dae37340b38fb0658900cb
Aryaman <aryaman1802@gmail.com>
2025-05-28 00:08:23 -0400

    hello world

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!\n"; 
+}
+
+// To run this file, go to the terminal and navigate to where this file is stored, and then type:
+// g++ -o otpt test.cpp
+// Press Enter, and then type:
+// ./otpt
